8099a3682917b04cc5905339691853152ac34e55
Haoze Huang <oliver.huang@mail.utoronto.ca>
2022-03-28 22:04:26 -0400

    added atlas cloud connection

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose')
 
 // Get the URI of the local database, or the one specified on deployment.
-const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/GameSquadAPI'
+const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://rachel:team47@csc309.ywd8x.mongodb.net/GameSquadAPI' || 'mongodb://localhost:27017/GameSquadAPI'
 
 // 'mongodb+srv://rachel:team47@csc309.ywd8x.mongodb.net/GameSquadAPI' 
 
